75b1614d57ea0b0ef20f0396f7652ddf2d26e4e3
federicov <vaglicafederico95@gmail.com>
2020-10-05 17:56:16 +0200

    Fine

--- dipendentiAzienda/dipendenti.php
+++ dipendentiAzienda/dipendenti.php
@@ -11,11 +11,11 @@ require_once __DIR__ .'/../traits/getResidenza.php';
     protected $salary;
 
     public function __construct($_name,$_surname,$_age,$_salary){
-        if($_name === '' && $_surname === ''){
+        if($_name === '' || $_surname === ''){
             die('inserisci un formato valido ');
         } 
-        if(!is_numeric($_age) && empty($_age)){
-            die('Devi inserie un formato valido per il salario ');
+        if(!is_numeric($_age) || empty($_age)){
+            die('Devi inserie un formato valido per l eta ');
         }
 
         $this->name = $_name;
